dee9c055e1077657b976c1fa60a6642b0534b60b
Jung9928 <fg43irks@naver.com>
2024-07-28 18:15:34 +0900

    환경설정 커밋

---
+++ src/main/java/com/skin_manager/skin_manager/SkinManagerApplication.java
@@ -0,0 +1,13 @@
+package com.skin_manager.skin_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkinManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkinManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skin_manager/skin_manager/SkinManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skin_manager.skin_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkinManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
